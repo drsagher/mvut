a280ee4aeefdba8e760f73228b87e6d71a4f544d
Abubakar Siddique <dr.sagher@gmail.com>
2024-05-11 05:37:27 -0700

    Dev v 0.0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import Image from "next/image";
-import Logo from "@/public/mvut_logo.png";
+import HeroPage from "@/app/components/hero";
+import CoursesPage from "@/app/components/courses";
+import SloganPage from "@/app/components/slogan";
+import NewsComponent from "@/app/components/news";
+import ProgramComponent from "@/app/components/programs";
+import ResearchComponent from "@/app/components/research";
+import FooterComponent from "@/app/components/footer";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Image src={Logo} alt="MVUT Logo" height={400}/>
-    <p className="text-5xl">Launching Soon ...</p>
+    <main className="flex min-h-screen flex-col items-center">
+      <HeroPage/>
+        <CoursesPage/>
+        <SloganPage/>
+      <NewsComponent/>
+        <ProgramComponent/>
+        <ResearchComponent/>
+        <FooterComponent/>
     </main>
   );
 }
